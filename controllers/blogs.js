865dde65acaecb7b19f7160cde8ca57684a7f11b
nohaiz <nohaiznazier@gmail.com>
2024-07-22 12:51:34 +0300

    Created the paths for index and create blogs. Updated the controller to render the templates

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const Blog = require('../models/blogs.js');
 
-const index = async ()=> {
-
+const index = async (req, res)=> {
+    res.render('blogs/index.ejs');
 }
-const newBlog = async ()=> {
-    
+const newBlog = async (req, res)=> {
+    res.render('blogs/create.ejs');
 }
 const updateBlog = async ()=> {
     
--- server.js
+++ server.js
@@ -10,12 +10,24 @@ require('./config/database.js');
 //Model 
 require('./models/blogs.js');
 
+//Controller
+const blogController = require('./controllers/blogs.js');
+
 //Middleware
 
 const app = express();
 app.use(morgan('dev'));
 
 //Routes
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
+//This wont work cause i dont have index ready yet
+app.get('/blogs',blogController.index)
+
+app.get('/blogs/create',blogController.newBlog)
+
 
 app.listen(3000, ()=> {
     console.log('Listening');
