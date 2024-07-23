17181d4aac927b8dab86cc1c08273a0aa83b4a1d
nohaiz <nohaiznazier@gmail.com>
2024-07-23 04:30:28 +0300

    Final product. Code clean up completed.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,21 +4,17 @@ const index = async (req, res)=> {
     const blogs = await Blog.find();
     res.render('blogs/index.ejs', {blogs});
 }
-
 const newBlog = async (req, res) => {
     res.render('blogs/create.ejs');
 }
-
 const createBlog = async (req, res)=> {
     await Blog.create(req.body);
     res.redirect('/blogs');
 }
-
 const showBlog = async (req, res) => {
-    const blogDetails = await Blog.findById(req.params);
-    res.render('blogs/show.ejs', {blogDetails});
+    const blog = await Blog.findById(req.params);
+    res.render('blogs/show.ejs', {blog});
 }
-
 const editBlog = async (req, res) => {
     const blog = await Blog.findById(req.params.id);
     res.render('blogs/update.ejs', {blog});
@@ -31,7 +27,6 @@ const deleteBlog = async (req, res)=> {
     await Blog.findByIdAndDelete(req.params.id)
     res.redirect('/blogs');
 }
-
 module.exports = {
     index,
     newBlog,
